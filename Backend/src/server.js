25d464d98da38649474e537b5695aec032f55a61
mayank-y1943 <mayankyada1943@gmail.com>
2025-07-11 16:10:34 +0530

    prepared for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,15 +4,21 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/ratelimit.js";
 import cors from 'cors';
+import path from "path"
 
 dotenv.config();
 
 const app=express();
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -23,6 +29,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(()=>{
     app.listen(5001, ()=>{
